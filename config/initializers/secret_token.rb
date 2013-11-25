94525c26d4a5db08cbca2b608f9b592891d4bddd
Maher Hawash <gmhawash@gmail.com>
2013-11-24 18:52:33 -0800

    first lesson

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuzzybook::Application.config.secret_key_base = 'bcb1cfe0057a89042780fbc34837a6aa783448c6bc16884b3e520110026e802793c952d5190239d9f97106477dadcd456a2ea9b0b69621005723294b77418702'
