e933afee270c7fc730a43361b423fd52318c6028
Maher Hawash <gmhawash@gmail.com>
2013-12-26 20:20:36 -0800

    Adding states to pages.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227034446) do
+ActiveRecord::Schema.define(version: 20131227040044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20131227034446) do
     t.datetime "updated_at"
     t.text     "message"
     t.text     "address"
+    t.string   "state"
   end
 
   create_table "posts", force: true do |t|
